b743f3ca4a8332d0811cd069d1e763f6e05347d0
luibelzan <luismi_madrilista@hotmail.com>
2021-12-08 20:26:06 +0100

    Test Authenticated level C done

--- src/test/java/acme/testing/anonymous/duty/AnonymousDutyListTest.java
+++ src/test/java/acme/testing/anonymous/duty/AnonymousDutyListTest.java
@@ -11,14 +11,25 @@ public class AnonymousDutyListTest extends AcmeEndeavoursTest{
 	@ParameterizedTest
 	@CsvFileSource(resources = "/anonymous/duty/list.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void ListRecent(final int recordIndex, final String title, final String initialPeriod, final String finalPeriod, final String description) {
+	public void ListRecent(final int recordIndex, final String title, final String periodInitial, final String periodFinal, final String description, final String workload, final String link) {
 
 		super.clickOnMenu("Anonymous", "List non finished duties");
 
 		super.checkColumnHasValue(recordIndex, 0, title);
-		super.checkColumnHasValue(recordIndex, 1, initialPeriod);
-		super.checkColumnHasValue(recordIndex, 2, finalPeriod);
+		super.checkColumnHasValue(recordIndex, 1, periodInitial);
+		super.checkColumnHasValue(recordIndex, 2, periodFinal);
 		super.checkColumnHasValue(recordIndex, 3, description);
+	
+		// Accedemos a un registro
+		super.clickOnListingRecord(recordIndex);
+
+		// Comprobamos cada valor
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("periodInitial", periodInitial);
+		super.checkInputBoxHasValue("periodFinal", periodFinal);
+		super.checkInputBoxHasValue("workloadInHours", workload);
+		super.checkInputBoxHasValue("link", link);
 
 	}
 
